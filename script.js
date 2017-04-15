1d65149e4a0f485797d59e6f71866e99e13940c6
Yannik Hodel <yannik.hodel@gmail.com>
2017-04-16 01:45:43 +0200

    Add mobile menu

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ $(document).ready(function(){
         $(".maintitlefooter").css("opacity", 1 - $(window).scrollTop() / 250);
     });
 });
+
+function moveTo(location) {
+	$('html, body').animate({
+    	scrollTop: $('#' + location).offset().top
+	}, 1000);
+}
+
+function openMobileMenu() {
+	$('.mobile-menu-open').animate({'border-spacing': 90}, {
+		step: function(now, fx) {
+			$('.mobile-menu-open').css('-webkit-transform', 'rotate(' + now + 'deg)');
+		},
+		duration: 400
+	}, 'linear');
+	$('.mobile-menu-open').promise().done(function() {
+		$('.mobile-menu-open').css('display', 'none');
+		$('.mobile-menu-close').css('display', 'block');
+		$('.mobile-menu').css('display', 'block');	
+	});
+}
+
+function closeMobileMenu() {
+	$('.mobile-menu-open').css({'-webkit-transform': 'rotate(0deg)', 'border-spacing': '0'});
+	$('.mobile-menu-open').css('display', 'block');
+	$('.mobile-menu-close').css('display', 'none');
+	$('.mobile-menu').css('display', 'none');
+}
