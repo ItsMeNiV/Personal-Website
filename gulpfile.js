430a342bf5c20dc5b9fa4a1991fa18a73458ff95
Yannik Hodel <yannik.hodel@gmail.com>
2017-04-26 00:35:15 +0200

    Add gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
+var watch = require('gulp-watch');
+
+gulp.task('watch', function() {
+  return watch('src/**/*', function () {
+    gulp.start('copysrc');
+    gulp.start('webpack');
+  });
+});
 
 gulp.task('default', function() {
   gulp.src(['src/index.html'])
@@ -11,6 +19,14 @@ gulp.task('default', function() {
   gulp.src(['src/images/**/*'])
         .pipe(gulp.dest('dist/static/images/'))
   gulp.start('webpack');
+});
+
+gulp.task("copysrc", function(callback) {
+  gulp.src(['src/index.html'])
+        .pipe(gulp.dest('dist/'))
+  gulp.src(['src/css/**/*'])
+        .pipe(gulp.dest('dist/static/css/'))
+  gulp.src(['src/images/**/*'])
 });
 
 gulp.task("webpack", function(callback) {
